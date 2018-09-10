eb4aa1eabef4617f1345cec62bb488904069fdc2
Oriva <weelman93@gmail.com>
2018-09-10 15:14:36 +0800

    save

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var red = document.querySelectorAll('.poloska');
-var doit, intervalloh, linePos = $('.mega-line').offset().top, lineAnimate = true, phototop, photoheight, wintop, winheight, worktop, workheight;
+var doit, intervalloh, linePos = $('.mega-line').offset().top, lineAnimate = true, phototop, photoheight, wintop, winheight, worktop, workheight, isClosed = false, music=new Audio("../img/hymn.mp3");
+
 $('.header__music .header__music_sound').click(function () {
 
     $('.header__music').toggleClass('playing'); //
@@ -10,6 +11,7 @@ $('.header__music .header__music_sound').click(function () {
     var animationDuration = 0.6;
 
     if(doit) {
+        music.play();
         red.forEach(function (element) {
             function setProperty(duration, size) {
                 element.style.setProperty('--animation-time', duration + 's');
@@ -23,6 +25,7 @@ $('.header__music .header__music_sound').click(function () {
             intervalloh;
         });
     } else {
+        music.pause();
         clearInterval(intervalloh);
     }
 
@@ -34,11 +37,19 @@ $('.header__music .header__music_sound').click(function () {
 //burger
 
 $('document').ready(function () {
-    var trigger = $('#hamburger'),
-        isClosed = false;
+    var trigger = $('#hamburger');
 
     trigger.click(function () {
-        burgerTime();
+        if(trigger.hasClass('disabled')) {
+            return false;
+        } else {
+            trigger.addClass('disabled');
+            burgerTime();
+            setTimeout(function () {
+                trigger.removeClass('disabled');
+                $('.item__icon-block').removeClass('hide');
+            }, 1400);
+        }
     });
 
     function burgerTime() {
@@ -46,16 +57,40 @@ $('document').ready(function () {
             trigger.removeClass('is-open');
             trigger.addClass('is-closed');
             isClosed = false;
+            timetoclock = 100;
+            for(i = 5; i >= 0; i--) {
+                (function (index){
+                    setTimeout(function () {
+                        $('.item__icon-block').eq(index).addClass('hide');
+                        $('.item__icon-block').eq(index).removeClass('show');
+                    }, timetoclock);
+                })(i);
+                timetoclock = timetoclock+200;
+                setTimeout(function () {
+                    $('.item').hide();
+                }, 1400);
+            };
         } else {
             trigger.removeClass('is-closed');
             trigger.addClass('is-open');
             isClosed = true;
+            $('.item').show();
+            timetoclock = 100;
+            for(i = 0; i < 5; i++) {
+                (function (index){
+                    setTimeout(function () {
+                        $('.item__icon-block').eq(index).addClass('show');
+                        $('.item__icon-block').eq(index).removeClass('hide');
+                    }, timetoclock);
+                })(i);
+                timetoclock = timetoclock+200;
+            };
         }
     }
 });
 
 $(document).scroll(function (e) {
-    if(($(window).scrollTop()+600)>linePos && lineAnimate==true) {
+    if((wintop+winheight/2)>linePos && lineAnimate==true && wintop<linePos) {
         $('.line-party').removeClass('line-not-ready');
         $('.mega-line').removeClass('line-not-ready');
         $('.text-party').addClass('text-ready');
@@ -69,7 +104,6 @@ $(document).scroll(function (e) {
 
     if((wintop+winheight)>(phototop) && wintop<phototop+photoheight && (!$('.photo-star__collage_item').hasClass('go-animate'))) {
         $('.photo-star__collage_item').addClass('go-animate');
-        console.log('123');
     } else if ((wintop+winheight)>(phototop) && wintop<phototop+photoheight) {
         return false;
     } else {
@@ -78,10 +112,25 @@ $(document).scroll(function (e) {
 
     if((wintop+winheight)>(worktop) && wintop<worktop+workheight && (!$('.working__collage_item').hasClass('go-animate'))) {
         $('.working__collage_item').addClass('go-animate');
-        console.log('123');
     } else if ((wintop+winheight)>(worktop) && wintop<worktop+workheight) {
         return false;
     } else {
         $('.working__collage_item').removeClass('go-animate');
     }
+});
+
+$('.scroll-weed-everyday').click(function (event) {
+    if($('.menu-fixed__other').hasClass('disabled')){
+        return false;
+    }
+    $('.menu-fixed__other').addClass('disabled');
+    event.preventDefault();
+    var id = $(this).data('href')
+        , top = $(id).offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 1000);
+    setTimeout(function () {
+        $('.menu-fixed__other').removeClass('disabled');
+    }, 1100);
 });
